041bab8cddb8a2655ef759ea5094d8d1e4f8c4de
Aska(work) <aska_kps@mail.ru>
2020-10-15 18:31:45 +0600

    work commit1

--- js/script.js
+++ js/script.js
@@ -19,5 +19,57 @@ $(".header__menu").on("click","a", function (event) {
 
 
 $( document ).ready(function() {
-    
+  var swiper = new Swiper('.reviews-slider', {
+    slidesPerView: 3,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next.reviews-arrow',
+      prevEl: '.swiper-button-prev.reviews-arrow',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 2,
+
+        },
+        // when window width is >= 640px
+        992: {
+          slidesPerView: 3,
+        }
+      },
+  });
+
+  // TRUST SCROLLBAR SLIDER
+
+
+
+  var swiper3 = new Swiper('.teachers-slider', {
+    slidesPerView: 3,
+    loop: false,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 2,
+
+        },
+        // when window width is >= 640px
+        992: {
+          slidesPerView: 3,
+        }
+      },
+  });
 });
+
+
